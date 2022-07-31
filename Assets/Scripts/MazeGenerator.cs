c6e33e3cc3f5553bfca0edaab3fb62a36af8910e
XDD44 <50290408+xdd44-utopia@users.noreply.github.com>
2022-07-31 20:54:27 +0800

    initialize maze

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+	public GameObject prefab;
+
+	private const int n = 5;
+	private const float cellSize = 3;
+
+	private int[] dx = {1, 0, -1, 0};
+	private int[] dy = {0, 1, 0, -1};
+	private Quaternion[] rots = {
+		Quaternion.Euler(0, 0, 90),
+		Quaternion.Euler(-90, 0, 0),
+		Quaternion.Euler(0, 0, -90),
+		Quaternion.Euler(90, 0, 0),
+		Quaternion.Euler(0, 0, 180),
+		Quaternion.Euler(0, 0, 0)
+	};
+	private Vector3[] poss = {
+		new Vector3(0.5f, 0.5f, 0),
+		new Vector3(0, 0.5f, 0.5f),
+		new Vector3(-0.5f, 0.5f, 0),
+		new Vector3(0, 0.5f, -0.5f),
+		new Vector3(0, 1, 0),
+		new Vector3(0, 0, 0)
+	};
+	private struct Cell {
+		public int idx;
+		public bool[] adj;
+		public int father;
+
+		public Cell(int i) {
+			idx = i;
+			adj = new bool[4]{false, false, false, false};
+			father = idx;
+		}
+	}
+	private Cell[] map = new Cell[n * n];
+
+	public Transform cam;
+	private int cx = 0;
+	private int cy = 0;
+	private int cd = 0;
+	private const float speed = 0.5f;
+	private bool isMoving = true;
+	private Vector3[] dv = {
+		new Vector3(1, 0, 0),
+		new Vector3(0, 0, 1),
+		new Vector3(-1, 0, 0),
+		new Vector3(0, 0, -1)
+	};
+	private const float turnSpeed = 2.5f;
+	private float[] dr = {90, 0, 270, 180};
+
+	void Start()
+	{
+		for (int i=0;i<n*n;i++) {
+			map[i] = new Cell(i);
+		}
+		for (int i=0;i<n*n-1;i++) {
+			int px = -1;
+			int py = -1;
+			int lx = -1;
+			int ly = -1;
+			int dir = -1;
+			int cnt = 0;
+			do {
+				px = Random.Range(0, n);
+				py = px == n - 1 ? Random.Range(0, n - 1) : Random.Range(0, n);
+				dir = px == n - 1 ? 1 : (py == n - 1 ? 0 : Random.Range(0, 2));
+				lx = px + dx[dir];
+				ly = py + dy[dir];
+				cnt++;
+			} while (find(px * n + py) == find(lx * n + ly) && cnt < 100);
+			union(px * n + py, lx * n + ly);
+			map[px * n + py].adj[dir] = true;
+			map[lx * n + ly].adj[dir + 2] = true;
+		}
+		for (int i=0;i<n*n;i++) {
+			for (int d=0;d<4;d++) {
+				if (!map[i].adj[d]) {
+					Instantiate(prefab, new Vector3(cellSize * (i / n), 0, cellSize * (i % n)) + poss[d] * cellSize, rots[d]);
+				}
+			}
+			Instantiate(prefab, new Vector3(cellSize * (i / n), 0, cellSize * (i % n)) + poss[4] * cellSize, rots[4]);
+			Instantiate(prefab, new Vector3(cellSize * (i / n), 0, cellSize * (i % n)) + poss[5] * cellSize, rots[5]);
+		}
+
+		cd = map[0].adj[0] ? 0 : 1;
+		cam.rotation = Quaternion.Euler(0, dr[cd], 0);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (isMoving) {
+			cam.position = cam.position + dv[cd] * speed * Time.deltaTime;
+			if (Vector3.Distance(cam.position, new Vector3((cx + dx[cd]) * cellSize, 1.5f, (cy + dy[cd]) * cellSize)) < 0.1f) {
+				cam.position = new Vector3((cx + dx[cd]) * cellSize, 1.5f, (cy + dy[cd]) * cellSize);
+				cx += dx[cd];
+				cy += dy[cd];
+				bool changeDir = false;
+				int prevcd = cd;
+				while (cx + dx[cd] < 0 || cy + dy[cd] < 0 || cx + dx[cd] >= n || cy + dy[cd] >= n || !map[(cx) * n + cy].adj[cd]) {
+					if (map[(cx) * n + cy].adj[prevcd ^ 1] || map[(cx) * n + cy].adj[prevcd ^ 1 ^ 2]) {
+						cd = Random.Range(0, 2) == 0 ? prevcd ^ 1 : prevcd ^ 1 ^ 2;
+					}
+					else {
+						cd = prevcd ^ 2;
+					}
+					changeDir = true;
+				}
+				isMoving = !changeDir;
+			}
+		}
+		else {
+			cam.rotation = Quaternion.Euler(0, Mathf.Lerp(cam.rotation.eulerAngles.y, dr[cd], turnSpeed * Time.deltaTime), 0);
+			if (Mathf.Abs(cam.rotation.eulerAngles.y - dr[cd]) < 0.025f) {
+				cam.rotation = Quaternion.Euler(0, dr[cd], 0);
+				isMoving = true;
+			}
+		}
+	}
+
+	private void union(int x, int y) {
+		if (find(x) != find(y)) {
+			map[find(x)].father = map[y].father;
+		}
+	}
+
+	private int find(int x) {
+		int p = map[x].father;
+		while (p != map[p].father) {
+			p = map[p].father;
+		}
+		map[x].father = p;
+		return p;
+	}
+}
